d4b1e41639422f9c7b89ceb57182954406a4bb92
Seongwoo Kim <kimsw@robotis.com>
2025-04-25 14:13:38 +0900

    corrected lint errors
    Signed-off-by: Seongwoo Kim <kimsw@robotis.com>

--- policy_to_trajectory/launch/policy_to_trajectory.launch.py
+++ policy_to_trajectory/launch/policy_to_trajectory.launch.py
@@ -20,13 +20,12 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
 from launch.substitutions import EqualsSubstitution
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 import yaml
-
 
 
 def load_yaml(path):
