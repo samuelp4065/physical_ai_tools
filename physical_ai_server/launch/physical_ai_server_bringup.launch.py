eccfd8e4685cb721a0e8955db999efbe08bdff85
ola31 <abrahamola80@gmail.com>
2025-07-02 18:18:51 +0900

    fix: ament flake8 lint
    Signed-off-by: ola31 <abrahamola80@gmail.com>

--- physical_ai_server/launch/physical_ai_server_bringup.launch.py
+++ physical_ai_server/launch/physical_ai_server_bringup.launch.py
@@ -22,8 +22,8 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 
 def generate_launch_description():
