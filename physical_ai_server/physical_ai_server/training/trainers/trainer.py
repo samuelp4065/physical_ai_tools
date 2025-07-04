49b53f306b89f015b41285c14f96dfa3ee04dbfd
Seongwoo Kim <kimsw@robotis.com>
2025-07-04 15:02:11 +0900

    Initial commit for training manager
    Signed-off-by: Seongwoo Kim <kimsw@robotis.com>

---
+++ physical_ai_server/physical_ai_server/training/trainers/trainer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# Copyright 2025 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Seongwoo Kim
+
+from abc import ABC, abstractmethod
+from typing import Dict, Any
+
+
+class Trainer(ABC):
+    
+    def __init__(self, config):
+        self.config = config
+        
+    @abstractmethod
+    def send_training_metrics(self) -> Dict[str, Any]:
+        """
+        Send training metrics to the server.
+        """
+        pass
+
+    @abstractmethod
+    def train(self):
+
+        pass
---
+++ physical_ai_server/physical_ai_server/training/training_manager.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+#
+# Copyright 2025 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Seongwoo Kim
+
+import os
+import torch
+
+from training.trainers.lerobot.lerobot_trainer import LerobotTrainer
+from training.trainers.grootn1.grootn1_trainer import GrootN1Trainer
+from training.trainers.openvla.openvla_trainer import OpenVLATrainer
+
+class TrainManager:
+    
+    def __init__(self, framework, config, device: str = 'cuda'):
+        self.framework = framework
+        self.config = config
+        self.device = device
+        self.trainer = self._get_trainer()
+        
+    def get_training_metrics(self):
+        metrics = self.trainer.send_training_metrics()
+        return metrics
+        
+
+    def _get_trainer(self):
+        if self.framework == "lerobot":
+            return LerobotTrainer(self.config)
+        elif self.framework == "grootn1":
+            return GrootN1Trainer(self.config)
+        elif self.framework == "openvla":
+            return OpenVLATrainer(self.config)
+        else:
+            raise ValueError(f"Unknown framework: {self.framework}")
+
+    def train(self):
+        self.trainer.train()
